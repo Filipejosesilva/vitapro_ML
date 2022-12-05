d3006a48ec504b3110de9975529a49e8d6f9c423
Filipejosesilva <118757194+Filipejosesilva@users.noreply.github.com>
2022-12-05 12:54:03 -0500

    Add files via upload

--- arnes2.py
+++ arnes2.py
@@ -22,10 +22,19 @@ with st.form(key='diabetes-pred-form'):
     col1, col2 = st.columns(2)
     
     arnes = col1.slider(label='Tamaño del arnés:', min_value=0, max_value=100)
-    botas = col2.text_input(label='Tamaño de la Bota:')
+    botas = col2.text_input(label='Tamaño de la Bota:', value = 0)
     submit = st.form_submit_button(label='Check')
     
     arnes = int(arnes)
     inputs = np.array(arnes).reshape(-1, 1)
     predicted_boot_size = loaded_model.predict(inputs)[0]
-    st.write("El modelo estima un tamaño de bota: ",round(predicted_boot_size))
+    #st.write("El modelo estima un tamaño de bota: ",round(predicted_boot_size))
+    predicted_boot_size = round(predicted_boot_size)
+    
+    if int(botas) == predicted_boot_size:
+        st.info("La bota es ideal")
+    elif int(botas) < predicted_boot_size:
+        st.info("La bota es muy pequeña")
+    else:
+        st.info("La bota es muy grande")
+        
